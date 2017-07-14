6332c21a0c8f336f441602593fe6a07f93cc6e73
Luke Oliff <luke@lukeoliff.com>
2017-07-14 23:31:38 +0100

    Oopsie

--- src/Methods/MethodInterface.php
+++ src/Methods/MethodInterface.php
@@ -6,6 +6,6 @@ namespace ZpgRtf\Methods;
  * An interface to make sure all methods to send data over to Zpg have
  * the relevant methods to do so.
  */
-class MethodInterface
+interface MethodInterface
 {
 }
