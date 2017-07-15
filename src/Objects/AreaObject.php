0026487881d0de219d82edae6d7c6bcf02acbbd2
Luke Oliff <luke@lukeoliff.com>
2017-07-15 14:20:38 +0100

    Add AreaObject object and test

--- src/Objects/AreaObject.php
+++ src/Objects/AreaObject.php
@@ -2,7 +2,60 @@
 
 namespace ZpgRtf\Objects;
 
-class AreaObject
+class AreaObject implements \JsonSerializable
 {
+    /** @var int **/
+    private $value;
 
+    /**
+     * Array of enum (sq_feet, sq_yards, sq_metres, acres, hectares)
+     *
+     * @var string e.g. sq_metres
+     * @todo Make these constants.
+     */
+    private $units;
+
+    /**
+     * @return int
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUnits()
+    {
+        return $this->units;
+    }
+
+    /**
+     * @param string $units
+     */
+    public function setUnits($units)
+    {
+        $this->units = $units;
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    public function jsonSerialize() {
+        return [
+            'units' => $this->getUnits(),
+            'value' => $this->getValue(),
+        ];
+    }
 }
---
+++ tests/Objects/AreaObjectTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use PHPUnit\Framework\TestCase;
+use ZpgRtf\Objects\AreaObject;
+
+class AreaObjectTest extends TestCase
+{
+    /**
+     * @var AreaObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new AreaObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            AreaObject::class,
+            $this->object
+        );
+    }
+
+    public function testCanSetValue()
+    {
+        $this->assertInstanceOf(
+            AreaObject::class,
+            $this->object->setValue(54.5)
+        );
+
+        $this->assertGreaterThan(
+            54.4,
+            $this->object->getValue()
+        );
+    }
+
+    public function testCanSetUnits()
+    {
+        $this->assertInstanceOf(
+            AreaObject::class,
+            $this->object->setUnits('sq_meters')
+        );
+
+        $this->assertStringStartsWith(
+            'sq_meters',
+            $this->object->getUnits()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+    }
+}
