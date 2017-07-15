f6b27db11b03a96d3aae2adca2e510c94d36aab6
Luke Oliff <luke@lukeoliff.com>
2017-07-15 15:41:36 +0100

    Add object and test for content type

--- src/Objects/ContentObject.php
+++ src/Objects/ContentObject.php
@@ -2,7 +2,88 @@
 
 namespace ZpgRtf\Objects;
 
-class ContentObject
+class ContentObject implements \JsonSerializable
 {
+    /** @var string */
+    private $url;
 
+    /**
+     * Enum(audio_tour, brochure, document, epc_graph, epc_report, floor_plan, home_pack, image, site_plan, virtual_tour)
+     *
+     * @var string
+     * @todo Make these constants.
+     */
+    private $type;
+
+    /** @var string */
+    private $caption;
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return ContentObject
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @return ContentObject
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCaption()
+    {
+        return $this->caption;
+    }
+
+    /**
+     * @param string $caption
+     *
+     * @return ContentObject
+     */
+    public function setCaption($caption)
+    {
+        $this->caption = $caption;
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    public function jsonSerialize() {
+        return [
+            'url' => $this->getUrl(),
+            'type' => $this->getType(),
+            'caption' => $this->getCaption(),
+        ];
+    }
 }
---
+++ tests/Objects/ContentObjectTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use PHPUnit\Framework\TestCase;
+use ZpgRtf\Objects\ContentObject;
+
+class ContentObjectTest extends TestCase
+{
+    /**
+     * @var ContentObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new ContentObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            ContentObject::class,
+            $this->object
+        );
+    }
+
+    public function testCanSetUrl()
+    {
+        $this->assertInstanceOf(
+            ContentObject::class,
+            $this->object->setUrl('https://www.github.com')
+        );
+
+        $this->assertStringStartsWith(
+            'https://www.github.com',
+            $this->object->getUrl()
+        );
+    }
+
+    public function testCanSetType()
+    {
+        $this->assertInstanceOf(
+            ContentObject::class,
+            $this->object->setType('image')
+        );
+
+        $this->assertStringStartsWith(
+            'image',
+            $this->object->getType()
+        );
+    }
+
+    public function testCanSetCaption()
+    {
+        $this->assertInstanceOf(
+            ContentObject::class,
+            $this->object->setCaption('An image.')
+        );
+
+        $this->assertStringStartsWith(
+            'An image.',
+            $this->object->getCaption()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+    }
+}
