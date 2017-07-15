08ea1d18a0e4b0453f397fcf9ddd3a73fb6561a8
Luke Oliff <luke@lukeoliff.com>
2017-07-15 18:51:08 +0100

    Add description object and test

--- src/Objects/DescriptionObject.php
+++ src/Objects/DescriptionObject.php
@@ -2,7 +2,86 @@
 
 namespace ZpgRtf\Objects;
 
-class DescriptionObject
+/**
+ * The description is expressed as an array of description objects, each of which represent a paragraph or section.
+ */
+class DescriptionObject implements \JsonSerializable
 {
+    /** @var string */
+    private $heading;
 
+    /** @var DimensionsObject */
+    private $dimensions;
+
+    /** @var string */
+    private $text;
+
+    /**
+     * @return string
+     */
+    public function getHeading()
+    {
+        return $this->heading;
+    }
+
+    /**
+     * @param string $heading
+     *
+     * @return DescriptionObject
+     */
+    public function setHeading($heading)
+    {
+        $this->heading = $heading;
+
+        return $this;
+    }
+
+    /**
+     * @return DimensionsObject
+     */
+    public function getDimensions()
+    {
+        return $this->dimensions;
+    }
+
+    /**
+     * @param DimensionsObject $dimensions
+     *
+     * @return DescriptionObject
+     */
+    public function setDimensions(DimensionsObject $dimensions)
+    {
+        $this->dimensions = $dimensions;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return DescriptionObject
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    public function jsonSerialize() {
+        return [
+            'heading' => $this->getHeading(),
+            'dimensions' => $this->getDimensions(),
+            'text' => $this->getText(),
+        ];
+    }
 }
---
+++ tests/Objects/DescriptionObjectTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use PHPUnit\Framework\TestCase;
+use ZpgRtf\Objects\DescriptionObject;
+use ZpgRtf\Objects\DimensionsObject;
+
+class DescriptionObjectTest extends TestCase
+{
+    /**
+     * @var DescriptionObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new DescriptionObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            DescriptionObject::class,
+            $this->object
+        );
+    }
+
+    public function testCanSetHeading()
+    {
+        $this->assertInstanceOf(
+            DescriptionObject::class,
+            $this->object->setHeading('Heading')
+        );
+
+        $this->assertSame(
+            'Heading',
+            $this->object->getHeading()
+        );
+    }
+
+    public function testCanTestDimensions()
+    {
+        $this->assertInstanceOf(
+            DescriptionObject::class,
+            $this->object->setDimensions(new DimensionsObject())
+        );
+
+        $this->assertInstanceOf(
+            DimensionsObject::class,
+            $this->object->getDimensions()
+        );
+    }
+
+    public function testCanSetText()
+    {
+        $this->assertInstanceOf(
+            DescriptionObject::class,
+            $this->object->setText('Text here')
+        );
+
+        $this->assertSame(
+            'Text here',
+            $this->object->getText()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+    }
+}
