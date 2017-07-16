cd801089045d71cb7516b26d5cfcd2a7e9ce751d
Luke Oliff <luke@lukeoliff.com>
2017-07-16 20:18:27 +0100

    Add tenant eligility object and test

--- src/Objects/TenantEligibilityObject.php
+++ src/Objects/TenantEligibilityObject.php
@@ -2,7 +2,72 @@
 
 namespace ZpgRtf\Objects;
 
-class TenantEligibilityObject
+/**
+ * This allows you to specify the eligibility of various classifications of applicant for a tenancy. Note that you do
+ * not have to specify eligibility for all classifications.
+ */
+class TenantEligibilityObject implements \JsonSerializable
 {
+    /**
+     * Enum (accepted, excluded, only)
+     *
+     * @var string
+     */
+    private $dss;
 
+    /**
+     * Enum (accepted, excluded, only)
+     *
+     * @var string
+     */
+    private $students;
+
+    /**
+     * @return string
+     */
+    public function getDss()
+    {
+        return $this->dss;
+    }
+
+    /**
+     * @param string $dss
+     *
+     * @return TenantEligibilityObject
+     */
+    public function setDss($dss)
+    {
+        $this->dss = $dss;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStudents()
+    {
+        return $this->students;
+    }
+
+    /**
+     * @param string $students
+     *
+     * @return TenantEligibilityObject
+     */
+    public function setStudents($students)
+    {
+        $this->students = $students;
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    public function jsonSerialize()
+    {
+        return [
+            'dss' => $this->getDss(),
+            'students' => $this->getStudents(),
+        ];
+    }
 }
---
+++ tests/Objects/TenantEligibilityObjectTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use PHPUnit\Framework\TestCase;
+use ZpgRtf\Objects\TenantEligibilityObject;
+
+class TenantEligibilityObjectTest extends TestCase
+{
+    /**
+     * @var TenantEligibilityObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new TenantEligibilityObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            TenantEligibilityObject::class,
+            $this->object
+        );
+    }
+
+    public function setDss()
+    {
+        $this->assertInstanceOf(
+            TenantEligibilityObject::class,
+            $this->object->setDss('excluded')
+        );
+
+        $this->assertSame(
+            'excluded',
+            $this->object->getDss()
+        );
+    }
+
+    public function setStudents()
+    {
+        $this->assertInstanceOf(
+            TenantEligibilityObject::class,
+            $this->object->setStudents('excluded')
+        );
+
+        $this->assertSame(
+            'excluded',
+            $this->object->getStudents()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+    }
+}
