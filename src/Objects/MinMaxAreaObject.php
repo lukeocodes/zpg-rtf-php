1b6290295760c63e162cbe061da5c103154faa47
Luke Oliff <luke@lukeoliff.com>
2017-07-16 19:25:21 +0100

    Add min max area object and test

--- src/Objects/MinMaxAreaObject.php
+++ src/Objects/MinMaxAreaObject.php
@@ -2,7 +2,64 @@
 
 namespace ZpgRtf\Objects;
 
-class MinMaxAreaObject
+/**
+ * The min_max_area object defines an area range. Where only one is specified, we will regard them both as having the
+ * same value - i.e. a fixed area.
+ */
+class MinMaxAreaObject implements \JsonSerializable
 {
+    /** @var AreaObject */
+    private $minimum;
 
+    /** @var AreaObject */
+    private $maximum;
+
+    /**
+     * @return AreaObject
+     */
+    public function getMinimum()
+    {
+        return $this->minimum;
+    }
+
+    /**
+     * @param AreaObject $minimum
+     *
+     * @return MinMaxAreaObject
+     */
+    public function setMinimum(AreaObject $minimum)
+    {
+        $this->minimum = $minimum;
+
+        return $this;
+    }
+
+    /**
+     * @return AreaObject
+     */
+    public function getMaximum()
+    {
+        return $this->maximum;
+    }
+
+    /**
+     * @param AreaObject $maximum
+     *
+     * @return MinMaxAreaObject
+     */
+    public function setMaximum(AreaObject $maximum)
+    {
+        $this->maximum = $maximum;
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    public function jsonSerialize()
+    {
+        return [
+            'minimum' => $this->getMinimum(),
+            'maximum' => $this->getMaximum(),
+        ];
+    }
 }
---
+++ tests/Objects/MinMaxAreaObjectTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use PHPUnit\Framework\TestCase;
+use ZpgRtf\Objects\AreaObject;
+use ZpgRtf\Objects\MinMaxAreaObject;
+
+class MinMaxAreaObjectTest extends TestCase
+{
+    /**
+     * @var MinMaxAreaObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new MinMaxAreaObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            MinMaxAreaObject::class,
+            $this->object
+        );
+    }
+
+    public function testCanSetMinimum()
+    {
+        $this->assertInstanceOf(
+            MinMaxAreaObject::class,
+            $this->object->setMinimum(new AreaObject())
+        );
+
+        $this->assertInstanceOf(
+            AreaObject::class,
+            $this->object->getMinimum()
+        );
+    }
+    
+    public function testCanSetMaximum()
+    {
+        $this->assertInstanceOf(
+            MinMaxAreaObject::class,
+            $this->object->setMaximum(new AreaObject())
+        );
+
+        $this->assertInstanceOf(
+            AreaObject::class,
+            $this->object->getMaximum()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+    }
+}
