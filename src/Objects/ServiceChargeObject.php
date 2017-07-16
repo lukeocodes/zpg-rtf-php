a4e567cbce730e074f9fc2768bc2ba5bf7ccfd86
Luke Oliff <luke@lukeoliff.com>
2017-07-16 20:12:44 +0100

    Add service charge object and test

--- src/Objects/ServiceChargeObject.php
+++ src/Objects/ServiceChargeObject.php
@@ -2,7 +2,96 @@
 
 namespace ZpgRtf\Objects;
 
-class ServiceChargeObject
+/**
+ * This object describes any ongoing service charges which are applicable. Note that you can only specify one of
+ * per_unit_area_units or frequency.
+ */
+class ServiceChargeObject implements \JsonSerializable
 {
+    /** @var float */
+    private $charge;
 
+    /**
+     * Enum (sq_feet, sq_yards, sq_metres, acres, hectares)
+     *
+     * @var string
+     */
+    private $perUnitAreaUnits;
+
+    /**
+     * Enum (per_person_per_week, per_week, per_month, per_quarter, per_year)
+     *
+     * @var string
+     */
+    private $frequency;
+
+    /**
+     * @return float
+     */
+    public function getCharge()
+    {
+        return $this->charge;
+    }
+
+    /**
+     * @param float $charge
+     *
+     * @return ServiceChargeObject
+     */
+    public function setCharge($charge)
+    {
+        $this->charge = $charge;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPerUnitAreaUnits()
+    {
+        return $this->perUnitAreaUnits;
+    }
+
+    /**
+     * @param string $perUnitAreaUnits
+     *
+     * @return ServiceChargeObject
+     */
+    public function setPerUnitAreaUnits($perUnitAreaUnits)
+    {
+        $this->perUnitAreaUnits = $perUnitAreaUnits;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFrequency()
+    {
+        return $this->frequency;
+    }
+
+    /**
+     * @param string $frequency
+     *
+     * @return ServiceChargeObject
+     */
+    public function setFrequency($frequency)
+    {
+        $this->frequency = $frequency;
+
+        return $this;
+    }
+
+    /** {@inheritDoc} */
+    public function jsonSerialize()
+    {
+        return [
+            'charge' => $this->getCharge(),
+            'per_unit_area_units' => $this->getPerUnitAreaUnits(),
+            'frequency' => $this->getFrequency(),
+        ];
+    }
 }
---
+++ tests/Objects/ServiceChargeObjectTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use PHPUnit\Framework\TestCase;
+use ZpgRtf\Objects\ServiceChargeObject;
+
+class ServiceChargeObjectTest extends TestCase
+{
+    /**
+     * @var ServiceChargeObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new ServiceChargeObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            ServiceChargeObject::class,
+            $this->object
+        );
+    }
+
+    public function testCanSetCharge()
+    {
+        $this->assertInstanceOf(
+            ServiceChargeObject::class,
+            $this->object->setCharge(100)
+        );
+
+        $this->assertSame(
+            100,
+            $this->object->getCharge()
+        );
+    }
+
+    public function testCanSetPerUnitAreaUnits()
+    {
+        $this->assertInstanceOf(
+            ServiceChargeObject::class,
+            $this->object->setPerUnitAreaUnits('sq_feet')
+        );
+
+        $this->assertSame(
+            'sq_feet',
+            $this->object->getPerUnitAreaUnits()
+        );
+    }
+
+    public function testCanSetFrequency()
+    {
+        $this->assertInstanceOf(
+            ServiceChargeObject::class,
+            $this->object->setFrequency('per_year')
+        );
+
+        $this->assertSame(
+            'per_year',
+            $this->object->getFrequency()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+    }
+}
