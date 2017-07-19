217544574c45e2731bc979d18f14513414dbd204
Luke Oliff <luke@lukeoliff.com>
2017-07-19 20:26:55 +0100

    Add listing delete object test

---
+++ tests/Objects/ListingDeleteObjectTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ZpgRtf\Tests\Objects;
+
+use ZpgRtf\Objects\ListingDeleteObject;
+use PHPUnit\Framework\TestCase;
+
+class ListingDeleteObjectTest extends TestCase
+{
+    /**
+     * @var ListingDeleteObject
+     */
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new ListingDeleteObject();
+    }
+
+    public function testCanInstantiate()
+    {
+        $this->assertInstanceOf(
+            ListingDeleteObject::class,
+            $this->object
+        );
+    }
+
+    public function testCanSetListingReference()
+    {
+        $this->assertInstanceOf(
+            ListingDeleteObject::class,
+            $this->object->setListingReference('listing-000001')
+        );
+
+        $this->assertSame(
+            'listing-000001',
+            $this->object->getListingReference()
+        );
+    }
+
+    public function testCanSetDeletionReason()
+    {
+        $this->assertInstanceOf(
+            ListingDeleteObject::class,
+            $this->object->setDeletionReason('Blah blah a reason')
+        );
+
+        $this->assertSame(
+            'Blah blah a reason',
+            $this->object->getDeletionReason()
+        );
+    }
+
+    public function testCanJsonSerialize()
+    {
+        $this->assertJson(
+            json_encode($this->object)
+        );
+
+        $this->assertInstanceOf(
+            \JsonSerializable::class,
+            $this->object
+        );
+    }
+}
